92f5daca3224808adef6002178bcb193fc60abdb
gedeeinstein <gedeeinstein@gmail.com>
2018-06-26 15:40:19 +0800

    Kategori.sql

---
+++ admin/kategori.sql
@@ -0,0 +1,37 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 10.1.31-MariaDB : Database - z_uas_web
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`z_uas_web` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `z_uas_web`;
+
+/*Table structure for table `kategori` */
+
+DROP TABLE IF EXISTS `kategori`;
+
+CREATE TABLE `kategori` (
+  `id_kategori` int(12) NOT NULL AUTO_INCREMENT,
+  `nm_kategori` varchar(120) NOT NULL,
+  `status` varchar(15) NOT NULL,
+  PRIMARY KEY (`id_kategori`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+
+/*Data for the table `kategori` */
+
+insert  into `kategori`(`id_kategori`,`nm_kategori`,`status`) values (1,'ALAT KERJA','active'),(2,'ALAT TULIS KANTOR','active'),(3,'BY OWNER','active'),(4,'CUSTOM','active'),(5,'DELUXE','active'),(6,'EQUIPMENT','active'),(7,'GETRA','active'),(8,'GWF','active'),(9,'MATERIAL','active'),(10,'PERALATAN','active'),(11,'PERLENGKAPAN','active'),(12,'PLAT','active'),(13,'RANGE','active');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
